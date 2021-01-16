8d5f5888d7af6d128ac98682bdcab278161481ef
Ali Morteza Yalmaztakin <alimurtaza7x@gmail.com>
2021-01-16 10:48:08 +0330

    On branch main

--- Basalam Proje/js/myscript.js
+++ Basalam Proje/js/myscript.js
@@ -1,79 +1,47 @@
-
-    var time1 = 1000;
-    var time2 = .9;
-    var time3 = 80;
-      down = null,
-      count = null,
-      timer = null;
-    function increment (time) {
-      time = (time * time2 > time3) ? (time * time2) : time3;
-      count.value ++ ;
-      timer = setTimeout(
-                function () {
-                  increment(time);
-                }, time);
+function loop1(o){
+    it = o[Symbol.iterator]();
+    return it.next.bind(it);}
+  var counter = document.getElementsByClassName('shoping-bottom');
+  var loop = function loop() {
+    var variable = step.value;
+    var minus = variable.getElementsByClassName('minus-btn');
+    var input = variable.querySelector('input');
+    var plus = variable.getElementsByClassName('plus-btn');
+    var inctimer = 0,dectimer = 0,counter = 0;
+    function increment() {
+      parseInt(input.value++);
+      counter += 1;
+      if (counter < 10) {
+        inctimer = setTimeout(function () {
+          increment();
+        }, 300);}
+        else {
+        inctimer = setTimeout(function () {
+          increment();
+        }, 30);
+      }
     }
-    function decrement (time) {
-      time = time * time2 > time3 ? (time * time2) : time3;
-     if(count.value > 0){
-       count.value --;}
-      timer = setTimeout(
-                function () {
-                  decrement(time);
-                }, time);
-   }
-   window.onload = function () {
-     count = document.getElementById('count');
-     document.onmouseup = function () {
-       clearTimeout(timer);
-     }
-   }
-   count1 = null;
-   count1 = document.getElementById('count1');
-   function increment1 (time) {
-    time = (time * time2 > time3) ? (time * time2) : time3;
-    count1.value ++ ;
-    timer = setTimeout(
-              function () {
-                increment1(time);
-              }, time);
-  }
-  function decrement1 (time) {
-    time = time * time2 > time3 ? (time * time2) : time3;
-   if(count1.value > 0){
-     count1.value --;}
-    timer = setTimeout(
-              function () {
-                decrement1(time);
-              }, time);
- }
-
-
-
-
- count2 = null;
- count2 = document.getElementById('count2');
- function increment2 (time) {
-  time = (time * time2 > time3) ? (time * time2) : time3;
-  count2.value ++ ;
-  timer = setTimeout(
-            function () {
-              increment2(time);
-            }, time);
-}
-function decrement2 (time) {
-  time = time * time2 > time3 ? (time * time2) : time3;
- if(count2.value > 0){
-   count2.value --;}
-  timer = setTimeout(
-            function () {
-              decrement2(time);
-            }, time);
-}
-
-
-  
-
-
- 
-  
+    plus[0].addEventListener('mousedown', increment);
+    plus[0].addEventListener('mouseup', clearTimer);
+    function decrement() {
+      if (input.value > 0) {
+        parseInt(input.value--);
+        counter += 100;}
+      if (counter < 1000) {
+        dectimer = setTimeout(function () {
+          decrement();
+        }, 300);
+      } else {
+        dectimer = setTimeout(function () {
+          decrement();
+        }, 30);
+      }
+    }
+    minus[0].addEventListener('mousedown', decrement);
+    minus[0].addEventListener('mouseup', clearTimer);
+    function clearTimer() {
+      clearTimeout(inctimer);
+      clearTimeout(dectimer);
+      counter = 0;}};
+  for (var _iterator = loop1(counter), step; !(step = _iterator()).done;) {
+    loop();}
